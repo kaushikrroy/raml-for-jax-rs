2747066febe09d284378c12f8894106b1f5aa416
jpbelang <jpbelang@gmail.com>
2018-03-02 18:44:47 -0500

    Ok, state of the union:  trying to get a jsonscema to return a better type. It currently returns the bad type.

--- raml-to-jaxrs/jaxrs-code-generator/src/main/java/org/raml/jaxrs/generator/JsonSchemaTypeGenerator.java
+++ raml-to-jaxrs/jaxrs-code-generator/src/main/java/org/raml/jaxrs/generator/JsonSchemaTypeGenerator.java
@@ -17,8 +17,10 @@ package org.raml.jaxrs.generator;
 
 import com.google.common.io.Files;
 import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.ParameterizedTypeName;
 import com.squareup.javapoet.TypeName;
 import com.sun.codemodel.JCodeModel;
+import com.sun.codemodel.JType;
 import org.jsonschema2pojo.*;
 import org.jsonschema2pojo.rules.RuleFactory;
 import org.raml.jaxrs.generator.builders.AbstractTypeGenerator;
@@ -30,6 +32,7 @@ import org.raml.jaxrs.generator.ramltypes.GType;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.List;
 
 /**
  * Created by Jean-Philippe Belanger on 11/20/16. Just potential zeroes and ones
@@ -42,6 +45,8 @@ public class JsonSchemaTypeGenerator extends AbstractTypeGenerator<JCodeModel> i
   private final ClassName name;
   private final String schema;
 
+  private JType model;
+
   public JsonSchemaTypeGenerator(CurrentBuild build, String pack, GType type, ClassName name, String schema) {
     this.build = build;
     this.pack = pack;
@@ -53,32 +58,52 @@ public class JsonSchemaTypeGenerator extends AbstractTypeGenerator<JCodeModel> i
   @Override
   public void output(CodeContainer<JCodeModel> container, BuildPhase buildPhase) throws IOException {
 
-    GenerationConfig config = build.getJsonMapperConfig();
-    final SchemaMapper mapper = new SchemaMapper(new RuleFactory(config, new Jackson2Annotator(), new SchemaStore()),
-                                                 new SchemaGenerator());
     final JCodeModel codeModel = new JCodeModel();
-
-    File schemaFile = File.createTempFile("schema", "json", build.getSchemaRepository());
-    Files.write(schema, schemaFile, Charset.defaultCharset());
-    try {
-
-      mapper.generate(codeModel, name.simpleName(), pack, schemaFile.toURL());
-    } catch (IOException e) {
-      throw new GenerationException(e);
+    JType model = createModel(codeModel);
+    if (!model.fullName().startsWith("java.util.List")) {
+      container.into(codeModel);
     }
-
-    container.into(codeModel);
   }
 
   public GType getType() {
     return type;
   }
 
+  private JType createModel(JCodeModel codeModel) throws IOException {
+
+    GenerationConfig config = build.getJsonMapperConfig();
+    final SchemaMapper mapper = new SchemaMapper(new RuleFactory(config, new Jackson2Annotator(), new SchemaStore()),
+                                                 new SchemaGenerator());
+
+    File schemaFile = File.createTempFile("schema", "json", build.getSchemaRepository());
+    Files.write(schema, schemaFile, Charset.defaultCharset());
+    try {
+
+      return mapper.generate(codeModel, name.simpleName(), pack, schemaFile.toURL());
+
+    } catch (IOException e) {
+      throw new GenerationException(e);
+    }
+  }
+
   @Override
   public TypeName getGeneratedJavaType() {
 
-    // duplicated logic with json2pojo. Should look in model.
-    return ClassName.get(name.packageName(), build.getJsonMapperConfig().getClassNamePrefix() + name.simpleName()
-        + build.getJsonMapperConfig().getClassNameSuffix());
+    try {
+      final JCodeModel codeModel = new JCodeModel();
+      JType model = createModel(codeModel);
+      // duplicated logic with json2pojo. Should look in model.
+      if (model.fullName().startsWith("java.util.List")) {
+
+        return ParameterizedTypeName
+            .get(ClassName.get(List.class), ClassName.get(name.packageName(), model.elementType().name()));
+      } else {
+
+        return ClassName.get(name.packageName(), build.getJsonMapperConfig().getClassNamePrefix() + name.simpleName()
+            + build.getJsonMapperConfig().getClassNameSuffix());
+      }
+    } catch (IOException e) {
+      throw new GenerationException(e);
+    }
   }
 }
